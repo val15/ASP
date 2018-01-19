1b5afd9f27ebf9683ec408d5c4b528ae99d1bf34
val15 <randrenarizoval@gmail.com>
2018-01-20 00:40:10 +0300

    dedut view

---
+++ TPFilRougeChoixResto/TPFilRougeChoixResto/Controllers/AccueilController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TPFilRougeChoixResto.Models;
+using TPFilRougeChoixResto.ViewModels;
+
+namespace TPFilRougeChoixResto.Controllers
+{
+    public class AccueilController : Controller
+    {
+        // GET: Accueil
+        public ActionResult Index()
+        {
+            AccueilViewModel vm = new AccueilViewModel
+            {
+                Message = "Bonjour depuis le contrôleur",
+                Date = DateTime.Now,
+                Resto = new Resto {Nom = "La bonne fourchette", Telephone = "1234"},
+
+                ListeDesRestos = new List<Resto>
+                {
+                    new Resto {Nom = "Resto pinambour", Telephone = "1234"},
+                    new Resto {Nom = "Resto tologie", Telephone = "1234"},
+                    new Resto {Nom = "Resto ride", Telephone = "5678"},
+                    new Resto {Nom = "Resto toro", Telephone = "555"},
+                },
+            };//par VM
+
+            //pour une liste dérolante : 
+            List<Models.Resto> listeDesRestos = new List<Resto>
+            {
+                new Resto { Id = 1, Nom = "Resto pinambour", Telephone = "1234" },
+                new Resto { Id = 2, Nom = "Resto tologie", Telephone = "1234" },
+                new Resto { Id = 5, Nom = "Resto ride", Telephone = "5678" },
+                new Resto { Id = 9, Nom = "Resto toro", Telephone = "555" },
+            };
+
+            ViewBag.ListeDesRestos = new SelectList(listeDesRestos, "Id", "Nom",5);
+            return View(vm);
+            }
+
+
+            //return View();
+
+    }
+}
---
+++ TPFilRougeChoixResto/TPFilRougeChoixResto/ViewModels/AccueilViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TPFilRougeChoixResto.ViewModels
+{
+    public class AccueilViewModel
+    {
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+        public Models.Resto Resto { get; set; }
+
+        public List<Models.Resto> ListeDesRestos { get; set; }
+    }
+}
